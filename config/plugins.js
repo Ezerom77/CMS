d2ea295de40d2cbef9a732b3c47e9408055032c5
Ezerom77 <fullstack@ezerom.com.ar>
2023-04-22 17:53:54 -0300

    First Commit

---
+++ config/plugins.js
@@ -0,0 +1,35 @@
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'info@ezerom.com.ar',
+        defaultReplyTo: 'info@ezerom.com.ar',
+      },
+    },
+  },
+  // ...
+});
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
